eded14e8017861adaccb44494b0e81f903e1236f
Chuck Mo <chuck@moosejudge.com>
2020-07-17 09:26:25 -0700

    test files

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+from moosecfg.utils import MooseDirs
+import pytest
+
+dirs = MooseDirs(name="snoopy")
+
+
+
+class TestMooseDirs:
+    def test_append_to_system_dirs(self):
+        assert False
+
+    def test_append_to_user_dirs(self):
+        assert False
